78a379813c60653902b41f6b66a63f2a44379d21
Kasper Hesthaven <kasper.hesthaven@dentsuaegis.com>
2020-07-30 13:51:30 +0200

    feat: Use stream & html-minifier-terser

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-const through2 = require("through2");
-const pluginError = require("plugin-error");
-const htmlMinifier = require("html-minifier");
+const htmlMinifier = require("html-minifier-terser");
+const PluginError = require("plugin-error");
+const Transform = require("stream").Transform;
+
+const PLUGIN_NAME = "gulp-html-minimizer";
 
 module.exports = (options = {}) => {
-	return through2.obj((file, encoding, callback) => {
-		if (file.isNull()) {
-			return callback(null, file);
-		}
+  return new Transform({
+    objectMode: true,
+    transform(chunk, encoding, callback) {
+      if (chunk._contents.length === 0 || chunk.isNull()) {
+        return callback(null, chunk);
+      }
 
-		if (file.isStream()) {
-			return callback(
-				new pluginError("gulp-html-minifier", "Streaming not supported")
-			);
-		}
-
-		(async () => {
-			try {
-				file.contents = Buffer.from(
-					htmlMinifier.minify(file.contents.toString(), options)
-				);
-				setImmediate(callback, null, file);
-			} catch (error) {
-				process.stderr.write(
-					`${new pluginError(
-						"gulp-html-minifier",
-						error.message
-					).toString()} \r\n`
-				);
-				return callback();
-			}
-		})();
-	});
+      if (chunk.isBuffer()) {
+        try {
+          chunk._contents = Buffer.from(
+            htmlMinifier.minify(chunk._contents.toString(encoding), options),
+            encoding
+          );
+          return callback(null, chunk);
+        } catch (error) {
+          if (options.skipInvalidFiles) {
+            process.stderr.write(
+              `${chunk.relative} failed parsing and has been skipped.\r\n`
+            );
+            return callback();
+          }
+          return callback(
+            new PluginError(PLUGIN_NAME, error, { fileName: chunk.relative })
+          );
+        }
+      } else if (chunk.isStream()) {
+        return callback(
+          new PluginError(PLUGIN_NAME, "Streaming is not supported.")
+        );
+      }
+    },
+  });
 };
